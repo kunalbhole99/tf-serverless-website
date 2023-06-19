72fca11fc0282179af83c25e7da6249a3d571a7a
kunalbhole99 <kunalbhole2699@gmail.com>
2023-06-19 12:11:07 +0530

    update

--- version-3/acm-certificate.tf
+++ version-3/acm-certificate.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "cert" {
-  provider                  = aws.use_default_region
+  provider                  = aws.north_virginia
   domain_name               = "*.${var.domain_name_simple}"
   validation_method         = "DNS"
   subject_alternative_names = [var.domain_name_simple]
@@ -13,7 +13,7 @@ resource "aws_acm_certificate" "cert" {
 ##################### AWS ACM Certificate validation ##################
 
 resource "aws_acm_certificate_validation" "cert" {
-  provider                = aws.use_default_region
+  provider                = aws.north_virginia
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
